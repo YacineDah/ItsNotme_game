5faddd8cb97f1d100f063b34ae9edc2974706d97
YacineDah <adayacineda@gmail.com>
2023-02-01 17:00:59 +0100

    vercel changes

--- itsnotme/itsnotme/settings.py
+++ itsnotme/itsnotme/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_o#@p3%ynyz9=swv%46(&whhew&q4)0@4+*u++%tj43l@hcd8h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -117,6 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
