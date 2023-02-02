8e3156a7867702a8ae7062921a78869984b31dbc
YacineDah <adayacineda@gmail.com>
2023-02-02 17:57:39 +0100

    style files

--- itsnotme/game/views.py
+++ itsnotme/game/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
 	return render(request, "home.html", {
-		"sheet": "/static/style.css",
+		"sheet": "{% static 'style.css' %}",
 	})
 	
 def play(request):
 	return render(request, "play.html", {
-		"sheet": "/static/play.css",
+		"sheet": "{% static 'play.css' %}",
 	})
